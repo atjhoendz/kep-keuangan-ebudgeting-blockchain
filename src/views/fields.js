8dfe6abdc9d5bf71467387ecaccd962e71441c3c
atjhoendz <achunarmando@gmail.com>
2021-04-19 11:05:55 +0700

    refactor: fields

---
+++ src/views/fields.js
@@ -0,0 +1,37 @@
+export const fieldsLembaga = [
+  { key: 'no', label: 'No.', _style: 'width:10px' },
+  { key: 'nama', label: 'Nama Lembaga' },
+  { key: 'jumlah_anggaran' },
+]
+
+export const fieldsAnggaran = [
+  { key: 'no', label: 'No.', _style: 'width:10px' },
+  'nama_lembaga',
+  'sisa_anggaran',
+]
+
+export const fieldsEstimasi = [
+  { key: 'no', label: 'No.', _style: 'width:10px' },
+  { key: 'tanggal_berangkat' },
+  { key: 'nama_pemohon' },
+  { key: 'nama_lembaga', label: 'Dari Lembaga' },
+  { key: 'jenis_pmk', label: 'Jenis PMK' },
+  { key: 'asal' },
+  { key: 'tujuan' },
+  { key: 'banyak' },
+  { key: 'keterangan' },
+  { key: 'total' },
+  { key: 'status_berkas' },
+]
+
+export const fieldsBiayaRiil = [
+  { key: 'no', label: 'No.', _style: 'width:10px' },
+  { key: 'tanggal_berangkat' },
+  { key: 'nama_pemohon' },
+  { key: 'nama_lembaga', label: 'Dari Lembaga' },
+  { key: 'jenis_pmk', label: 'Jenis PMK' },
+  { key: 'asal' },
+  { key: 'tujuan' },
+  { key: 'total' },
+  { key: 'keterangan' },
+]
