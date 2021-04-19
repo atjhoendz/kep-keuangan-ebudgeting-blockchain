4c615e86c185682c9c4ee0a2697bd15babced6d8
atjhoendz <achunarmando@gmail.com>
2021-04-19 11:06:11 +0700

    feat(lembaga): implement API lembaga

--- src/views/LaporanLembaga.vue
+++ src/views/LaporanLembaga.vue
@@ -3,34 +3,64 @@
     <CCol sm="12" md="6" lg="5">
       <card-list-data
         title="Laporan Lembaga"
-        :items="itemsLembaga"
-        :fields="fields"
+        :items="items"
+        :fields="fieldsLembaga"
         routeEndpoint="laporan-lembaga"
+        :isLoading="isLoading"
       ></card-list-data>
     </CCol>
+    <toast-msg :listToasts="listToasts" />
   </CRow>
 </template>
 
 <script>
-import CardListData from "../components/CardListData.vue";
-import { itemsLembaga } from "../sample-data/data";
-
-const fields = [
-  { key: "no", label: "No.", _style: "width:10px" },
-  "nama-lembaga",
-  "jumlah-anggaran",
-];
+import CardListData from '../components/CardListData.vue'
+import ToastMsg from '../components/ToastMsg'
+import { LaporanService } from '../services/laporan.service'
+import { fieldsLembaga } from './fields'
 
 export default {
-  name: "LaporanLembaga",
-  components: { CardListData },
+  name: 'LaporanLembaga',
+  components: { CardListData, ToastMsg },
   data() {
     return {
-      itemsLembaga,
-      fields,
-    };
+      items: [],
+      fieldsLembaga,
+      listToasts: [],
+      isLoading: false,
+    }
   },
-};
+  methods: {
+    async getDataLembaga() {
+      this.isLoading = true
+      try {
+        const dataLembaga = await LaporanService.getLembagaData()
+
+        this.items = dataLembaga.map(item => {
+          item.Record.jumlah_anggaran = parseInt(
+            item.Record.jumlah_anggaran
+          ).toLocaleString('id', {
+            style: 'currency',
+            currency: 'IDR',
+          })
+          return {
+            key: item.Key,
+            ...item.Record,
+          }
+        })
+      } catch (err) {
+        this.listToasts.push({
+          message: 'Terjadi masalah. Data lembaga tidak berhasil didapatkan.',
+          color: 'danger',
+        })
+      }
+      this.isLoading = false
+    },
+  },
+  async mounted() {
+    await this.getDataLembaga()
+  },
+}
 </script>
 
 <style></style>
